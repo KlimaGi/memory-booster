946429d53115af3182cb65cacdd2d1dafe06c1b6
Giedre <giedre.klima@gmail.com>
2022-01-22 21:22:50 +0200

    init, back, front-basic

---
+++ backend/server.js
@@ -0,0 +1,75 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+const corsOptions = {
+  origin: "http://localhost:8081",
+};
+
+app.use(cors(corsOptions));
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// open Mongoose connection to MongoDB database
+const db = require("./models");
+const Role = db.role;
+
+db.mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DB}`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    initial();
+  })
+  .catch((err) => {
+    console.error("Connection error", err);
+    process.exit();
+  });
+// create rows in roles collection
+initial = () => {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: "user",
+      }).save((err) => {
+        if (err) console.log("error", err);
+        console.log("added 'user' to roles collection");
+      });
+
+      new Role({
+        name: "admin",
+      }).save((err) => {
+        if (err) console.log("error", err);
+        console.log("added 'admin' to roles collection");
+      });
+    }
+  });
+};
+
+// route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to memory-booster application." });
+});
+app.get("/login", (req, res) => {
+  res.json({ message: "You are loged in to memory-booster application." });
+});
+
+// routes
+require("../backend/routes/auth.routes")(app);
+require("../backend/routes/user.routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
