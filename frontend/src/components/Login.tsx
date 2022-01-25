058001ee4b2a4cf1b4fd65b1ce353e1a605dce7c
Giedre <giedre.klima@gmail.com>
2022-01-25 22:51:20 +0200

    authentication service

---
+++ frontend/src/components/Login.tsx
@@ -0,0 +1,3 @@
+import React, { useState } from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
---
+++ frontend/src/services/auth-header.ts
@@ -0,0 +1,11 @@
+export const authHeader = () => {
+  const userStr = localStorage.getItem("user");
+  let user = null;
+  if (userStr) user = JSON.parse(userStr);
+
+  if (user && user.accessToken) {
+    return user.accessToken;
+  } else {
+    return;
+  }
+};
